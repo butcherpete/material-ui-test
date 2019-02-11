00919148ab1bdcdd1595f86592825d6ccacae97a
butcherpete <magpie.picaraza@gmail.com>
2019-02-11 15:03:04 -0800

    Update appjsx

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,16 +1,60 @@
 import React, { Component, Fragment } from "react";
 import Header from "./Layouts/Header";
 import Footer from "./Layouts/Footer";
-
+import { muscles, exercises } from "../store.js";
 import Exercises from "./Exercises/Exercises";
 
 export default class extends Component {
+  state = {
+    exercises,
+    exercise: {}
+  };
+
+  getExercisesByMuscles() {
+    return Object.entries(
+      this.state.exercises.reduce((exercises, exercise) => {
+        const { muscles } = exercise;
+
+        exercises[muscles] = exercises[muscles]
+          ? [...exercises[muscles], exercise]
+          : [exercise];
+
+        return exercises;
+      }, {})
+    );
+  }
+  handleCategorySelected = category => {
+    this.setState({
+      category
+    });
+  };
+
+  handleExerciseSelected = id => {
+    this.setState(({ exercises }) => ({
+      exercise: exercises.find(ex => ex.id === id)
+    }));
+  };
+
   render() {
+    const exercises = this.getExercisesByMuscles(),
+      { category, exercise } = this.state;
+
     return (
       <Fragment>
         <Header />
-        <Exercises />
-        <Footer />
+
+        <Exercises
+          exercise={exercise}
+          category={category}
+          exercises={exercises}
+          onSelect={this.handleExerciseSelected}
+        />
+
+        <Footer
+          category={category}
+          muscles={muscles}
+          onSelect={this.handleCategorySelected}
+        />
       </Fragment>
     );
   }
