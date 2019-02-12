5324f62723b394443db2f54b424df85a7c107620
butcherpete <magpie.picaraza@gmail.com>
2019-02-11 17:57:24 -0800

    mide way through 4

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -36,8 +36,6 @@ export default class extends Component {
   };
 
   render() {
-    console.log(this.getExercisesByMuscles());
-
     const exercises = this.getExercisesByMuscles(),
       { category, exercise } = this.state;
 
--- src/Components/Layouts/Footer.jsx
+++ src/Components/Layouts/Footer.jsx
@@ -3,13 +3,29 @@ import Paper from "@material-ui/core/Paper";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 
-export default ({ muscles }) => (
-  <Paper>
-    <Tabs value={0} indicatorColor="primary" textColor="primary" centered>
-      <Tab label="All" />
-      {muscles.map(group => (
-        <Tab key={group} label={group} />
-      ))}
-    </Tabs>
-  </Paper>
-);
+export default ({ muscles, category, onSelect }) => {
+  const index  = category 
+  ? muscles.findIndex(group => group === category + 1)
+  : 0
+
+  const onIndexSelect = (e, index) =>
+    onSelect(index === 0 ? '' : muscles[index - 1])
+    <Fragment>
+      <Paper>
+        <Tabs
+          value={0}
+          indicatorColor="primary"
+          textColor="primary"
+          centered
+          onChange={onIndexSelect}
+        >
+          <Tab label="All" />
+          {muscles.map(group => (
+            <Tab key={group} label={group} />
+          ))}
+        </Tabs>
+      </Paper>
+    </Fragment>
+}
+ 
+
